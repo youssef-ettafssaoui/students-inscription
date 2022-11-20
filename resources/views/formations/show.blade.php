5bb0bac17f8097fc3ff9951503e9dafce2da786b
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-11-20 14:47:52 +0100

    done

--- resources/views/formations/show.blade.php
+++ resources/views/formations/show.blade.php
@@ -23,7 +23,7 @@
 
     <div class="intro">
         <div class="intro_background parallax-window" data-parallax="scroll"
-            data-image-src="{{ asset('front/images/formation-header.jpg') }}" data-speed="0.8"></div>
+            data-image-src="{{ asset('front/images/formation-header.png') }}" data-speed="0.8"></div>
         <div class="container">
             <div class="row">
                 <div class="col">
